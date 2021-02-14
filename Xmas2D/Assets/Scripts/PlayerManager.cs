004f4b5ea161a78092bce322d26b05a17671e0a2
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-02-14 14:23:56 -0600

    Update PlayerManager.cs
    Fixed formatting errors on previous upload.
    Looking back on this code several years later, my documentation is nearly non-existent. 
    Thankfully that's a problem I've grown better about on modern projects.

--- Xmas2D/Assets/Scripts/PlayerManager.cs
+++ Xmas2D/Assets/Scripts/PlayerManager.cs
@@ -1,30 +1,83 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class PlayerManager : MonoBehaviour {     public float speedX;     public float jumpSpeedY;      bool facingRight;     bool jumping;      float speed;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    SpriteRenderer spr;     Animator anim;     Rigidbody2D rb;      int prevState;     int currState; 	// Use this for initialization 	void Start ()     {         spr = GetComponent<SpriteRenderer>();          // Stores a reference to the Animator component inside anim.         anim = GetComponent<Animator>();         // Stores a reference to the Rigidbody2D component inside rb.         rb = GetComponent<Rigidbody2D>();          // Initially player is facing right.         facingRight = true;         jumping = false;         prevState = 0;         currState = 0; 	} 	 	// Update is called once per frame 	void Update ()     {         // player movement         MovePlayer(speed);          // left player movement         if (Input.GetKeyDown(KeyCode.LeftArrow))         {             if (facingRight)
+public class PlayerManager : MonoBehaviour
+{    
+	public float speedX;    
+	public float jumpSpeedY;    
+	bool facingRight;    
+	bool jumping;    
+	float speed;
+
+    SpriteRenderer spr;    
+	Animator anim;    
+	Rigidbody2D rb;    
+	int prevState;    
+	int currState;	
+	
+	// Use this for initialization
+	void Start ()    
+	{        
+		spr = GetComponent<SpriteRenderer>();
+		// Stores a reference to the Animator component inside anim.        
+		anim = GetComponent<Animator>();
+		// Stores a reference to the Rigidbody2D component inside rb.        
+		rb = GetComponent<Rigidbody2D>();
+		
+		// Initially player is facing right.     
+		facingRight = true;  
+		jumping = false; 
+		prevState = 0; 
+		currState = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// player movement    
+		MovePlayer(speed);
+		
+		// left player movement
+		if (Input.GetKeyDown(KeyCode.LeftArrow))     
+		{
+			if (facingRight)
+			{
+				spr.flipX = true;
+				facingRight = false;
+			}
+			prevState = currState;
+			currState = 2;
+			anim.SetInteger("State", currState);         
+			speed = -speedX;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			if (!facingRight)
             {
-                spr.flipX = true;
-                facingRight = false;
-            }
-            prevState = currState;
-            currState = 2;
-            anim.SetInteger("State", currState);             speed = -speedX;         }         if (Input.GetKeyUp(KeyCode.LeftArrow))         {             if (!facingRight)
-            {
-                // we don't want character to move anymore
+				// we don't want character to move anymore
                 speed = speedX + speed;
                 prevState = currState;
                 currState = 0;
                 anim.SetInteger("State", currState);
-            }         }
+			}
+		}
 
         // right player movement
-        if (Input.GetKeyDown(KeyCode.RightArrow))         {             if (!facingRight)
+        if (Input.GetKeyDown(KeyCode.RightArrow))    
+		{   
+			if (!facingRight)
             {
                 spr.flipX = false;
                 facingRight = true;
             }
             prevState = currState;
             currState = 2;
-            anim.SetInteger("State", currState);             speed = speedX;         }         if (Input.GetKeyUp(KeyCode.RightArrow))         {
+            anim.SetInteger("State", currState);         
+			speed = speedX;    
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow))  
+		{
             if (facingRight)
             {
                 // we don't want character to move anymore
@@ -32,7 +85,8 @@
                 prevState = currState;
                 currState = 0;
                 anim.SetInteger("State", currState);
-            }         }
+            }
+		}
 
         // jump movement
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -45,6 +99,7 @@
                 rb.AddForce(new Vector2(rb.velocity.x, jumpSpeedY));
             }
         }
+		
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             prevState = currState;
@@ -58,15 +113,22 @@
             anim.SetInteger("State", currState);
         }
         
-    }      void MovePlayer(float playerSpeed)     {
+    }
+	
+	void MovePlayer(float playerSpeed)
+	{
         if (!jumping)
         {
             // code for player movement
             rb.velocity = new Vector3(speed, rb.velocity.y, 0);
-        }         else
+        }
+		else
         {
             rb.velocity = new Vector3(speed / 2, rb.velocity.y, 0);
-        }     }      void OnCollisionEnter2D(Collision2D collision)
+        }
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision)
     {
         if (jumping)
         {
@@ -79,4 +141,4 @@
             }
         }
     }
- } 
+}
